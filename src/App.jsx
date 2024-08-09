5fc82c7bce5e954ce410e7082d2d4f92e8d5186b
gabriela <gabysiiu@hotmail.com>
2024-08-09 14:04:52 -0700

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Nav />
       <Outlet />
+      <Footer/>
     </>
   );
 }
