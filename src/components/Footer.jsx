c3a781830879d89223ba3b6d17f212e78e1c53a7
gabriela <gabysiiu@hotmail.com>
2024-08-09 14:42:54 -0700

    resume created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
             &larr; Go Back
           </button>
         )}
-        <h4>&copy; {new Date().getFullYear()} - Tech Friends</h4>
+        <h4>&copy; {new Date().getFullYear()} - Gabriela Siu</h4>
       </div>
     </footer>
   );
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,76 +1,12 @@
 //resume
 // contact
-export default function ResumePage (){
-    return (
-        <div className="container pt-4">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-            tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-            impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-            architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-            adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-            mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-            nostrum temporibus ad omnis nam rerum eligendi.
-          </p>
-          <section className="features-icons bg-light text-center m-4">
-            <div className="container">
-              <div className="row p-2">
-                <div className="col-lg-4">
-                  <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                    <div className="features-icons-icon d-flex">
-                      <i className="bi-window m-auto text-primary" />
-                    </div>
-                    <h3>Fully Responsive</h3>
-                    <p className="lead mb-0">
-                      This theme will look great on any device, no matter the size!
-                    </p>
-                  </div>
-                </div>
-                <div className="col-lg-4">
-                  <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                    <div className="features-icons-icon d-flex">
-                      <i className="bi-layers m-auto text-primary" />
-                    </div>
-                    <h3>Bootstrap 5 Ready</h3>
-                    <p className="lead mb-0">
-                      Featuring the latest build of the new Bootstrap 5 framework!
-                    </p>
-                  </div>
-                </div>
-                <div className="col-lg-4">
-                  <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                    <div className="features-icons-icon d-flex">
-                      <i className="bi-terminal m-auto text-primary" />
-                    </div>
-                    <h3>Easy to Use</h3>
-                    <p className="lead mb-0">
-                      Ready to use with your own content, or customize the source
-                      files!
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-            tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-            impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-            architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-            adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-            mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-            nostrum temporibus ad omnis nam rerum eligendi.
-          </p>
-          return (
+export default function ResumePage() {
+  return (
     <div className="container pt-4">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
+        Enthusiastic Fullstack Developer Bootcamp student with solid knowledge
+        in JavaScript, APIs, Node.js, and various other technologies.
+        Seeking an entry-level position to apply my skills in a dynamic environment and contribute to innovative projects.
       </p>
       <section className="features-icons bg-light text-center m-4">
         <div className="container">
@@ -80,9 +16,23 @@ export default function ResumePage (){
                 <div className="features-icons-icon d-flex">
                   <i className="bi-window m-auto text-primary" />
                 </div>
-                <h3>Fully Responsive</h3>
+                <h3>Education</h3>
                 <p className="lead mb-0">
-                  This theme will look great on any device, no matter the size!
+                  Full stack web developer Virtual Bootcamp
+                  March, 2024 - September 2024
+
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                <div className="features-icons-icon d-flex">
+                  <i className="bi-window m-auto text-primary" />
+                </div>
+                <h3>Key Topics</h3>
+                <p className="lead mb-0">
+                  JavaScript, APIs, Node.js, Object-Oriented Programming, Express, SQL, ORM, MVC, NoSQL, PWA, React
+
                 </p>
               </div>
             </div>
@@ -91,38 +41,66 @@ export default function ResumePage (){
                 <div className="features-icons-icon d-flex">
                   <i className="bi-layers m-auto text-primary" />
                 </div>
-                <h3>Bootstrap 5 Ready</h3>
+                <h3>Technical Skills</h3>
                 <p className="lead mb-0">
-                  Featuring the latest build of the new Bootstrap 5 framework!
+                  Languages: JavaScript Frameworks/Libraries: Node.js, Express, React Databases: SQL, NoSQL Concepts: Object-Oriented Programming, MVC, ORM, PWA, APIs Tools: [Include any relevant tools, e.g., Git, Docker]
                 </p>
               </div>
             </div>
-            <div className="col-lg-4">
+            {/* <div className="col-lg-4">
               <div className="features-icons-item mx-auto mb-0 mb-lg-3">
                 <div className="features-icons-icon d-flex">
                   <i className="bi-terminal m-auto text-primary" />
                 </div>
-                <h3>Easy to Use</h3>
+                <h3>Projects</h3>
                 <p className="lead mb-0">
-                  Ready to use with your own content, or customize the source
-                  files!
+                  Projects
+                  Project Name
+                  [Brief Description]
+
+                  Developed using JavaScript, Node.js, Express, and React
+                  Implemented [specific feature or functionality]
+                  Utilized SQL/NoSQL databases to manage data
+                  Followed MVC architecture and employed ORM for database interactions
+                  
+                  Project Name
+                  [Brief Description]
+
+                  Created a Progressive Web App (PWA) to [brief description of functionality]
+                  Integrated external APIs to [describe integration]
+                  Focused on responsive design and user experience
+                  ols: [Include any relevant tools, e.g., Git, Docker]
+
                 </p>
               </div>
-            </div>
+            </div> */}
+
           </div>
         </div>
       </section>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
-      </p>
+
+      <div className="container pt-4">
+        <p>
+          Projects
+          Project Name
+          [Brief Description]
+
+          Developed using JavaScript, Node.js, Express, and React
+          Implemented [specific feature or functionality]
+          Utilized SQL/NoSQL databases to manage data
+          Followed MVC architecture and employed ORM for database interactions
+          Project Name
+          [Brief Description]
+
+          Created a Progressive Web App (PWA) to [brief description of functionality]
+          Integrated external APIs to [describe integration]
+          Focused on responsive design and user experience
+
+        </p>
+
+
+      </div>
     </div>
-  );  </div>
-      );
+  );
 
 }
